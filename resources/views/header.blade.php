71ffe6c4887fd592c94945cb592cb16a4a21e964
Mohaimenul Haque <haquerion2000@gmail.com>
2026-03-18 05:40:11 +0600

    Adjust spacing and add second product section/image
    Reduce vertical spacing in CSS (layout_padding and .shop_section .btn-box) to tighten layout; add new banner image public/frontend/img/img2.jpg. Cleanup minor whitespace in header.blade.php. Rework resources/views/welcome.blade.php: remove commented heading, change product card bottom margin (mb-3 -> mb-1), comment out original View All button, add a new full-width image section and a second shop section that reuses the product loop to display product cards with image, price, discount and Add to Cart button; legacy contact form was removed/kept commented. These changes create a more compact product grid and introduce a visual banner section.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -14,7 +14,6 @@
             </button>
 
             <div class="container m-0 p-0">
-                
                 <div class="collapse navbar-collapse bg-dark" id="navbarSupportedContent">
                     <ul class="navbar-nav  ">
                         <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,11 +22,6 @@
     <!-- shop section -->
     <section class="shop_section layout_padding">
         <div class="container">
-            {{-- <div class="heading_container heading_center">
-                <h3>
-                    Latest Products
-                </h3>
-            </div> --}}
 
             <div class="d-flex align-items-center">
                 <div class="flex-grow-1 border-top"></div>
@@ -34,6 +29,80 @@
                 <div class="flex-grow-1 border-top"></div>
             </div>
             
+            <div class="row">
+
+                @foreach ($getLatestProduct as $product)
+
+                    <div class="col-4 col-sm-6 col-md-4 col-lg-3 col-xl-2 px-1 mb-1">
+                        <div class="box">
+
+                            <a href="#">
+                                <div class="img-box mb-2">
+                                    <img src="{{ asset('upload/products/' . $product->img) }}" alt="">
+
+                                    <div class="overlay">
+                                        <p class="view-product">View Product</p>
+                                    </div>
+
+                                    <div class="new">
+                                        <span>New</span>
+                                    </div>
+                                </div>
+                            </a>
+
+
+                            <div class="bottom-part d-flex flex-column justify-center text-center">
+
+                                <h6 class="m-0 product_name">{{ $product->product_name }}</h6>
+                                <h6>
+                                    <span class="price">৳{{ $product->price }}</span>
+
+                                    @if ($product->regular_price)
+                                        <span class="regular_price">
+                                            ৳<s>{{ $product->regular_price }}</s>
+                                        </span>
+                                    @endif
+                                </h6>
+
+                                <div class="discount">
+                                    <span>20%</span>
+                                </div>
+
+                                <button class="btn btn-dark w-100">Add to Cart</button>
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                @endforeach
+
+            </div>
+            {{-- <div class="btn-box">
+                <a href="">
+                    View All Products
+                </a>
+            </div> --}}
+        </div>
+    </section>
+    <!-- end shop section -->
+
+
+    
+    <!-- 2nd shop section -->
+    <section class="container p-0">
+        <div class="row">
+            <div class="col-12">
+                <img class="img-fluid w-100" src="frontend/img/img2.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="shop_section layout_padding">
+        <div class="container">
+
             <div class="row">
 
                 @foreach ($getLatestProduct as $product)
@@ -85,65 +154,15 @@
                 @endforeach
 
             </div>
-            <div class="btn-box">
+            {{-- <div class="btn-box">
                 <a href="">
                     View All Products
                 </a>
-            </div>
+            </div> --}}
         </div>
     </section>
     <!-- end shop section -->
 
-
-
-    {{-- <!-- contact section -->
-    <section class="contact_section ">
-        <div class="container px-0">
-            <div class="heading_container ">
-                <h2 class="">
-                    Contact Us
-                </h2>
-            </div>
-        </div>
-        <div class="container container-bg">
-            <div class="row">
-                <div class="col-lg-7 col-md-6 px-0">
-                    <div class="map_container">
-                        <div class="map-responsive">
-                            <iframe
-                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
-                                width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%"
-                                allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-5 px-0">
-                    <form action="#">
-                        <div>
-                            <input type="text" placeholder="Name" />
-                        </div>
-                        <div>
-                            <input type="email" placeholder="Email" />
-                        </div>
-                        <div>
-                            <input type="text" placeholder="Phone" />
-                        </div>
-                        <div>
-                            <input type="text" class="message-box" placeholder="Message" />
-                        </div>
-                        <div class="d-flex ">
-                            <button>
-                                SEND
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <br><br><br>
-    <!-- end contact section --> --}}
 
 
 
